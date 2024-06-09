efe60bd9075e4c92212e8c26fc57992aada723a0
Danu313 <muwildanuus@students.amikom.ac.id>
2024-06-09 12:13:06 +0700

    first commit

---
+++ src/components/Home/About.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="py-5">
+    <div class="container px-5">
+      <div class="row gx-5 justify-content-center">
+        <div class="col-xxl-8">
+          <div class="text-center my-5">
+            <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">About Me</span></h2>
+            <p class="lead fw-light mb-4">My name is Muhammad Wildanu Staifen, students at <a href="amikom.ac.id"
+                class="text-decoration-none text-gradient" target="_blank">Amikom</a>
+              <!-- & <a href="https://duniacoding.id/" class="text-decoration-none text-gradient" target="_blank">Dunia Coding</a>.-->
+            </p>
+            <p class="text-muted">
+              I have an interest in development technology. 
+              I am also quite active in taking part in bootcamps on the internet, 
+              I am always learning to follow along technological development
+            </p>
+            <div class="d-flex justify-content-center fs-2 gap-4">
+              <a class="text-gradient" href="https://github.com/Danu313" target="_blank"><i
+                  class='bx bxl-github'></i></a>
+              <a class="text-gradient" href="https://www.linkedin.com/in/muhammad-wildanu-staifen-81106428b/"
+                target="_blank"><i class='bx bxl-linkedin'></i></a>
+              <a class="text-gradient" href="https://www.instagram.com/threadripper_000" target="_blank"><i
+                  class='bx bxl-instagram'></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
